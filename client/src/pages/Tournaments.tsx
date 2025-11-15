755771cf63563e575b850c2c3e16b7da7b0891ad
Ivannn <109225439+PastasciuttaEtonno@users.noreply.github.com>
2025-11-15 11:24:27 +0100

    Update Tournaments.tsx

--- client/src/pages/Tournaments.tsx
+++ client/src/pages/Tournaments.tsx
@@ -690,7 +690,7 @@ export default function Tournaments() {
 
   return (
     <div className="flex flex-col min-h-screen bg-background pb-20">
-      <PageHeader title="Tournament" action={<HeaderLogo />} />
+      <PageHeader title="Tornei" action={<HeaderLogo />} />
 
       <main className="flex-1 px-4 py-6 max-w-2xl mx-auto w-full">
         {/* Single list view; admins can edit combos from player dialog */}
