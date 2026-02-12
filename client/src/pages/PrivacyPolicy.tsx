47086a8d2ab3d1491ec04d4a3f799ef08828c763
Ivannn <109225439+PastasciuttaEtonno@users.noreply.github.com>
2026-02-12 12:10:34 +0100

    Update PrivacyPolicy.tsx

--- client/src/pages/PrivacyPolicy.tsx
+++ client/src/pages/PrivacyPolicy.tsx
@@ -122,33 +122,7 @@ export default function PrivacyPolicy() {
                             </p>
                         </div>
 
-                        <div className="pt-4 border-t space-y-4">
-                            <div>
-                                <p className="font-medium">10. Ezoic Services</p>
-                                <p>
-                                    This website uses the services of Ezoic Inc. (“Ezoic”), including to manage third-party interest-based advertising. Ezoic may employ a variety of technologies on this website, including tools to serve content, display advertisements and enable advertising to visitors of this website, which may utilize first and third-party cookies.
-                                </p>
-                                <p className="mt-2">
-                                    A cookie is a small text file sent to your device by a web server that enables the website to remember information about your browsing activity. First-party cookies are created by the site you are visiting, while third-party cookies are set by domains other than the one you're visiting. Ezoic and our partners may place third-party cookies, tags, beacons, pixels, and similar technologies to monitor interactions with advertisements and optimize ad targeting. Please note that disabling cookies may limit access to certain content and features on the website, and rejecting cookies does not eliminate advertisements but will result in non-personalized advertising. You can find more information about cookies and how to manage them <a href="https://allaboutcookies.org/" target="_blank" rel="noopener noreferrer" className="underline text-primary">here</a>.
-                                </p>
-                                <p className="mt-2">
-                                    The following information may be collected, used, and stored in a cookie when serving personalized ads:
-                                </p>
-                                <ul className="list-disc pl-5 mt-1 space-y-1">
-                                    <li>IP address</li>
-                                    <li>Operating system type and version</li>
-                                    <li>Device type</li>
-                                    <li>Language preferences</li>
-                                    <li>Web browser type</li>
-                                    <li>Email (in a hashed or encrypted form)</li>
-                                </ul>
-                                <p className="mt-2">
-                                    Ezoic and its partners may use this data in combination with information that has been independently collected to deliver targeted advertisements across various platforms and websites. Ezoic’s partners may also gather additional data, such as unique IDs, advertising IDs, geolocation data, usage data, device information, traffic data, referral sources, and interactions between users and websites or advertisements, to create audience segments for targeted advertising across different devices, browsers, and apps. You can find more information about interest-based advertising and how to manage them <a href="https://youradchoices.com/" target="_blank" rel="noopener noreferrer" className="underline text-primary">here</a>.
-                                </p>
-                                <p className="mt-2">
-                                    You can view Ezoic’s privacy policy <a href="https://ezoic.com/privacy/" target="_blank" rel="noopener noreferrer" className="underline text-primary">here</a>, or for additional information about Ezoic’s advertising and other partners, you can view Ezoic’s advertising partners <a href="https://www.ezoic.com/privacy-policy/advertising-partners/" target="_blank" rel="noopener noreferrer" className="underline text-primary">here</a>.
-                                </p>
-                            </div>
+                        <div className="pt-4 border-t">
                             <span id="ezoic-privacy-policy-embed"></span>
                         </div>
                     </div>
